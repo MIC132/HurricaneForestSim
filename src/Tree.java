8763647fdd27a0c57b603fd2837feeb5b381d9a2
MIC132 <mic132mic@gmail.com>
2014-11-16 18:41:42 +0100

    Refined Forest - Tree interaction

--- src/Tree.java
+++ src/Tree.java
@@ -1,6 +1,8 @@
 import java.util.concurrent.Semaphore;
 
 public abstract class Tree extends Agent{
+    double x;
+    double y;
     double MOR;
     double MOE;
     double airDens = 1.226;
@@ -15,11 +17,16 @@ public abstract class Tree extends Agent{
     double crownW;
     double rootM;
     double rootD;
+
     Forest forest;
+
+    volatile double windSpeed;
+    volatile double avgHeight;
+    volatile double avgDist;
 
     State state = State.OK;
 
-    public enum State{OK, TOPPLED, UPROOTED}
+    public enum State{OK, BROKEN, UPROOTED}
 
     Semaphore sem = new Semaphore(0);
 
@@ -31,8 +38,11 @@ public abstract class Tree extends Agent{
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
-
-
+            if(getTotalForce() > getTopplePoint()){
+                state = State.BROKEN;
+            }else if(getTotalForce()>getBreakingPoint()){
+                state = State.UPROOTED;
+            }
             forest.sem.release();
         }
     }
@@ -49,10 +59,9 @@ public abstract class Tree extends Agent{
         return (1/2)*dragCoeff*airDens*Math.pow(speed,2)*getSurface(height);
     }
 
-    double getFGust(double s, double h, double x){
-        double GMean = Math.pow(((0.68 * (s / h)) - 0.0385) + (((-0.68 * (s / h)) + 0.4875) * ((1.7239 * (s / h)) + 0.0316)),x/h);
-        double GMax = Math.pow(((2.7913 * (s / h)) - 0.061) + (((-1.273 * (s / h)) + 9.9701) * ((1.1127 * (s / h)) + 0.0311)),x/h);
-
+    double getFGust(double x){
+        double GMean = Math.pow(((0.68 * (avgDist / avgHeight)) - 0.0385) + (((-0.68 * (avgDist / avgHeight)) + 0.4875) * ((1.7239 * (avgDist / avgHeight)) + 0.0316)),x/avgHeight);
+        double GMax = Math.pow(((2.7913 * (avgDist / avgHeight)) - 0.061) + (((-1.273 * (avgDist / avgHeight)) + 9.9701) * ((1.1127 * (avgDist / avgHeight)) + 0.0311)),x/avgHeight);
         return GMax/GMean;
     }
 
@@ -68,8 +77,16 @@ public abstract class Tree extends Agent{
         return (gravity*rootM*rootD)/soilToTreeRatio;
     }
 
-    double getBMax(double height, double speed){
-        return getFGust(1, 1, 1) * getFGap() * (getWindForce(height, speed) + (getGravForce(height) * getDisplacement(height, speed)));
+    double getBMax(double height){
+        return getFGust(1) * getFGap() * (getWindForce(height, windSpeed) + (getGravForce(height) * getDisplacement(height, windSpeed)));
+    }
+
+    double getTotalForce(){
+        double total = 0;
+        for(int i=0;i<height;i++){
+            total += getBMax(i);
+        }
+        return total;
     }
 
     double getDisplacement(double height, double speed){
@@ -77,5 +94,9 @@ public abstract class Tree extends Agent{
                 / ((6 * MOE * Math.PI * Math.pow(getSurface(1.3), 4)) / 64);
         return (getWindForce(height, speed) * Math.pow(crownH, 3) * ((2 - ((3 * ((this.height - height) - (crownH - this.height))) / crownH)) + (Math.pow(this.height - height - (crownH - this.height), 3) / Math.pow(crownH, 3))))
                 / ((6 * MOE * Math.PI * Math.pow(getSurface(1.3), 4)) / 64);
+    }
+
+    double getDistance(Tree tree){
+        return Math.sqrt(Math.pow(this.x - tree.x, 2) + Math.pow(this.y - tree.y, 2) );
     }
 }
