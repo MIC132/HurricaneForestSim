3be9fa643623ebc91db3c62ab128813de2e96d53
HrabiaVulpes <hrabiavulpes@gmail.com>
2014-11-16 21:47:01 +0100

    I created some shit, probably wrong one

--- src/NorwaySpruce.java
+++ src/NorwaySpruce.java
@@ -1,8 +1,4 @@
 public class NorwaySpruce extends Tree {
-    // dwie
-    // linijki
-    // komentarza
-
     public NorwaySpruce(int x, int y, double trunkH, double trunkW, double crownH, double crownW) {
         this.x = x;
         this.y = y;
@@ -12,25 +8,26 @@ public class NorwaySpruce extends Tree {
         this.crownH = crownH;
         this.crownW = crownW;
         this.rootD = height/2;
-        this.dens = 550;                // kg/m^3
+        this.dens = 430;                // kg/m^3
         this.rootM = 1/5 * Math.pow(crownH, 3) * dens;
-        this.MOE = 7000;
-        this.MOR = 39.1;
-        this.dragCoeff = 0.29;
-        this.soilToTreeRatio = 0.3;
+        this.MOE = 6300;
+        this.MOR = 30.6;
+        this.dragCoeff = 0.35;
+        this.soilToTreeRatio = 0.2;
+    }
+
+    double get_dist(double height){
+        if(height >= 0 && height < trunkH) return trunkW;
+        else if(height >= trunkH && height < this.height) return 2*(this.height-height)*(crownW/(2*crownH));
+        else return 0;
     }
 
     @Override
     double getSurface(double height) {
-        if (height > this.height || height < 0) return 0;
-        else if (height + 1 <= trunkH) return trunkW * 1;            //trunkW * 1m
-        else if (height < trunkH && height + 1 > trunkH)
-            return (trunkH - height) * trunkW + ((Math.pow(height + 1 - trunkH, 2) / crownH) * Math.sqrt(Math.pow(height + 1 - trunkH, 2) + Math.pow(crownW / 2, 2)) * Math.sin(Math.atan(crownW / (2*crownH))));
-        else if (height >= trunkH && height + 1 < height)
-            return ((Math.pow(height + 1 - trunkH, 2) / crownH) * Math.sqrt(Math.pow(height + 1 - trunkH, 2) + Math.pow(crownW / 2, 2)) * Math.sin(Math.atan(crownW / (2*crownH))))
-                    - ((Math.pow(height - trunkH, 2) / crownH) * Math.sqrt(Math.pow(height - trunkH, 2) + Math.pow(crownW / 2, 2)) * Math.sin(Math.atan(crownW / (2*crownH))));
-        else if(height >= trunkH && height+1 >this.height)
-            return ((Math.pow(this.height-height, 2) / crownH) * Math.sqrt(Math.pow(this.height-height, 2) + Math.pow(crownW / 2, 2)) * Math.sin(Math.atan(crownW / (2*crownH))));
-        else return 0;
+        double wynik = 0;
+        for(double x = height; x < height+1; x+= 0.01){
+            wynik += 0.01*get_dist(x);
+        }
+        return wynik;
     }
 }
